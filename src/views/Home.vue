1a9ba5ebcd3d39ff9486f6c53f70ab5ebd854258
fakherBaccouch <59736278+fakherBaccouch@users.noreply.github.com>
2020-01-27 16:08:15 +0100

    update layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,49 +2,58 @@
   <div id="app">
     <v-row style="height:100vh">
       <v-col
-        style="background-color:#985E6D"
+        style="background-color:#e67e22"
         cols="12"
         sm="12"
-        md="4"
-        class="d-flex flex-column justify-space-between align-center"
+        md="2"
+        class="d-flex flex-column justify-space-between align-center pr-0 pb-0"
       >
         <div>
-          <div style="width:150px" class="pb-4">
-            <v-img style="border-radius:50%" src="../assets/myphoto.jpg"></v-img>
+          <div style="width:150px">
+            <v-img src="../assets/myphoto.jpg"></v-img>
           </div>
-          <div align="center" class="white--text">
-            <p>Fakher baccouch</p>
-            <p>Vuejs frontend developer</p>
+          <div class="black--text">
+            <p>
+              <strong>Fakher baccouch</strong>
+            </p>
+            <p class="mb-0 mt-5">Age:21</p>
+            <p>HTML,CSS,JS,VueJs</p>
           </div>
         </div>
-        <div align="center">
-          <v-btn x-large style="background-color:#98878F color:white">Save</v-btn>
-        </div>
+
+        <v-btn style="width:100%" x-large color="success ">Sauvegarder</v-btn>
       </v-col>
       <v-col
         cols="12"
         sm="12"
-        md="8"
+        md="10"
         class="d-flex flex-column justify-center"
-        style="background-color:#192231"
+        style="background-color:#40739e"
+        id="list"
       >
-        <h1 style="text-align:center " class="display-3 align-center white--text">My Skills</h1>
+        <h1 style="text-align:center " class="display-3 align-center black--text">Je suis un titre</h1>
+        <p style="text-align:center " class="display-1 align-center black--text">Je suis un soutitre</p>
         <br />
         <br />
-        <v-list-item-title style="color:#fff;width:200px">
-          <v-btn fab small class="mr-4">1</v-btn>HTML
+
+        <v-list-item-title style="color:#fff;">
+          <v-btn fab small style="font-size:30px" class="mr-8">1</v-btn>
+          <span>Element 1</span>
         </v-list-item-title>
 
-        <v-list-item-title style="color:#fff;width:200px">
-          <v-btn fab small class="mr-4">3</v-btn>CSS
+        <v-list-item-title style="color:#fff">
+          <v-btn fab small style="font-size:30px" class="mr-8">2</v-btn>
+          <span>Element 2</span>
         </v-list-item-title>
 
-        <v-list-item-title style="color:#fff;width:200px">
-          <v-btn fab small class="mr-4">4</v-btn>Javascript
+        <v-list-item-title style="color:#fff">
+          <v-btn fab small style="font-size:30px" class="mr-8">3</v-btn>
+          <span>Element 3</span>
         </v-list-item-title>
 
-        <v-list-item-title style="color:#fff;width:200px">
-          <v-btn fab small class="mr-4">5</v-btn>VueJs
+        <v-list-item-title style="color:#fff">
+          <v-btn fab small style="font-size:30px" class="mr-8">4</v-btn>
+          <span>Element 4</span>
         </v-list-item-title>
       </v-col>
     </v-row>
@@ -56,4 +65,14 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+#list span {
+  background: white;
+  color: black;
+  border-radius: 5px;
+  height: 200px;
+  width: 700px;
+  font-size: 30px;
+  padding-right: 150px;
+  border: 1px solid black;
+}
 </style>
